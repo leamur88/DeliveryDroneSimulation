faaa9b5aa44c73e1e5ffe36733d9a4dc4d60c0ff
Varun Reddy <reddy215@umn.edu>
2021-04-05 14:42:02 -0500

    fix(delivery_paths.h): fix #51 (description of commit) - initiailzing header file

---
+++ project/include/parabolicpath.h
@@ -0,0 +1,33 @@
+/**
+ * @file parabolicpath.h
+ */
+#ifndef PARABOLIC_PATH_H_
+#define PARABOLIC_PATH_H_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "entity_base.h"
+#include "drone.h"
+#include "robot.h"
+namespace csci3081 {
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+	/**
+	 * @brief This serves as an intermediary class for all of the objects that pickup and deliver packages.
+	 *
+	 * This class should never actually be instantiated as a real object. It can be used to help dynamic 
+     * casting. On top of that, it helps keep the DRY priniciple in place for our code. 
+	 */
+	class ParabolicPath : public csci3081::iDeliveryPaths{
+		public:
+            virtual void UpdatePath() = 0;
+
+		protected:
+			Drone* d;
+            Robot* r;
+		};
+
+}
+#endif
