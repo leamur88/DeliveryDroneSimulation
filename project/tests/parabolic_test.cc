382bdcf897fb4a3b8b4933f075d48f56d1c81a8a
Jules Mockovak <mocko026@umn.edu>
2021-04-19 18:10:56 -0500

    fix(parabolic_test.cc): fix #78 (Added Test for GetPath)
    -Added new TEST_F for the GetPath function

--- project/tests/parabolic_test.cc
+++ project/tests/parabolic_test.cc
@@ -88,14 +88,70 @@ class ParabolicTest : public ::testing::Test {
 				ASSERT_FLOAT_EQ(custroute[i][j], testcustroute[i][j]);
 			}
 		}
-
-
-
-
-
-
-
-
-
 	}
+
+	TEST_F(ParabolicTest, GetPath){
+		ParabolicPath path;
+		std::vector<float> position_to_add1;
+		position_to_add1.push_back(490.5);
+		position_to_add1.push_back(-100.3);
+		position_to_add1.push_back(43.1);
+
+		std::vector<float> position_to_add3;
+		position_to_add3.push_back(-150);
+		position_to_add3.push_back(400.2);
+		position_to_add3.push_back(550);
+
+		std::vector<std::vector<float>> Route = path.GetPath(position_to_add1, position_to_add3);
+
+		std::vector<std::vector<float>> tempRoute;
+		Vector3D begin(position_to_add1);
+		Vector3D finish(position_to_add3);
+		Vector3D distance = finish - begin;
+
+		float increment =  distance.Magnitude()/100;
+		float Start_Mid_Magnitude = (distance.Magnitude())/2; 
+
+		//find the unit vector
+		Vector3D unit = distance;
+		unit.Normalize();
+
+		std::vector<float> mid = {(position_to_add3[0] - position_to_add1[0])/2, (position_to_add3[1] - position_to_add1[1])/2, (position_to_add3[2] - position_to_add1[2])/2};
+		Vector3D midpoint(mid);
+		midpoint = midpoint + begin;
+
+		//loop through the x axis in incriments of distance/100, assigning the height based on a parabolic formula
+		tempRoute.push_back(position_to_add1);
+		for (int i = 1; i < 101; i++){
+			//next position on x axis
+			std::vector<float> next_pos = {i*increment * unit.GetVector()[0], i*increment * unit.GetVector()[1], i*increment * unit.GetVector()[2]};
+			Vector3D next(next_pos);
+			next = next + begin;
+			next_pos = next.GetVector();
+
+			Vector3D step = next - midpoint;
+			float Next_Mid_Magnitude = step.Magnitude();
+			float y = (1 - (pow(Next_Mid_Magnitude,2)/pow(Start_Mid_Magnitude,2))) * 300;
+			next_pos[1] += y;
+
+			tempRoute.push_back(next_pos);
+		}
+
+		for (int i = 0; i < Route.size(); i++){
+			for (int j = 0; i < 3; i++){
+				ASSERT_FLOAT_EQ(Route[i][j], tempRoute[i][j]);
+			}
+		}
+	}
+
+	
+
+
+
+
+
+
+
+
+
 }
